940605be19de3ca43c3011ce6c8bd2650c26d241
Shivam Shukla <Shivamshukla@keybrainstech.com>
2025-03-13 19:01:34 +0530

    Font change

--- api/app.js
+++ api/app.js
@@ -32,10 +32,7 @@ app.set("view engine", "jade");
 
 // app.use(cors());
 app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
+  cors()
 );
 
 app.use(logger("dev"));
