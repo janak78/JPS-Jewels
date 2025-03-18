ad5d9e7a51e6210688e9425b95f59cd726e904a3
MitMangukiya <mitmangukiya192@gmail.com>
2025-03-18 19:10:07 +0530

    m_fast_160325

--- api/app.js
+++ api/app.js
@@ -30,7 +30,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
 // app.use(cors());
-app.use(cors({ origin: "http://localhost:4000" }));
+app.use(cors({ origin: "*" }));
 
 app.use(logger("dev"));
 app.use(express.json());
