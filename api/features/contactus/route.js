427143c2e837dfcdc6a16b67ad70eff11e808275
Shivam Shukla <Shivamshukla@keybrainstech.com>
2025-04-02 09:52:54 +0530

    login loader

--- api/features/contactus/route.js
+++ api/features/contactus/route.js
@@ -76,9 +76,9 @@ const addContact = async (data) => {
     body { font-family: Arial, sans-serif; background-color: #f4f4f4; color: #333; margin: 0; padding: 0; }
     table { width: 100%; border-spacing: 0; }
     .email-container { max-width: 600px; margin: 20px auto; background: #fff; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); }
-    .email-header { background: rgb(172, 130, 80); color: #fff; font-size: 24px; text-align: center; padding: 20px; border-bottom: 3px solid #0056b3; }
+    .email-header { background: #c9a236; color: #fff; font-size: 24px; text-align: center; padding: 20px; border-bottom: 3px solid #000; }
     .email-body { padding: 20px; }
-    .email-body h2 { color: rgb(172, 130, 80); margin: 0 0 10px; }
+    .email-body h2 { color: #c9a236; margin: 0 0 10px; }
     .email-body p { font-size: 16px; line-height: 1.6; margin: 0 0 10px; }
     .email-footer { background: #f1f1f1; text-align: center; padding: 15px; font-size: 14px; color: #777; border-top: 1px solid #ddd; }
     .email-footer a { color: #007BFF; text-decoration: none; }
